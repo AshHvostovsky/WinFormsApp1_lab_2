87b444fc9b68269405bb4cf720bdfc3a729aa38d
AshHvostovsky <ash.stdw@gmail.com>
2024-03-18 22:43:53 +0800

    ADD

--- WinFormsApp1_lab_2/Form1.Designer.cs
+++ WinFormsApp1_lab_2/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 118);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(label2);
+            flowLayoutPanel1.Location = new Point(12, 12);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(200, 100);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(194, 30);
+            label2.TabIndex = 2;
+            label2.Text = "label2fffffffffffffffffffffffffffffffffffffffffffffff";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(109, 118);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label label2;
+        private TextBox textBox1;
     }
 }
--- WinFormsApp1_lab_2/Form1.cs
+++ WinFormsApp1_lab_2/Form1.cs
@@ -4,7 +4,95 @@ namespace WinFormsApp1_lab_2
     {
         public Form1()
         {
-            InitializeComponent();//testfix
+            InitializeComponent();
+            // считывем значения из настроек
+            txtBoxMoney.Text = Properties.Settings.Default.money_in.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int money_in = 0;
+            try
+            { // оборачиваем кусок кода склонный к падению
+              // Это должно показать сообщение с текстом
+                money_in = int.Parse(this.txtBoxMoney.Text);
+
+            }
+            catch (FormatException)
+            {
+                txtBoxMoney.Text = "";
+                MessageBox.Show("Неверно введены данные", "Результат");
+                returnFocus();
+
+                return;
+            }
+            string msg = Logic.GetOutput(money_in);
+            // выведем сообщение
+            MessageBox.Show(msg, "Результат");
+            returnFocus();
+            txtBoxMoney.Text = "";
+
+
+            //  передаем введенные значения в параметры
+            Properties.Settings.Default.money_in = money_in;
+            Properties.Settings.Default.Save(); // сохраняем переданные значения, чтобы они восстановились пре очередном запуске
+        }
+
+        private void returnFocus()
+        {
+            txtBoxMoney.Focus(); // Устанавливаем фокус обратно на текстовое поле
+        }
+
+
+        private void txtBoxMoney_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //button1_Click(this, e);
+                SelectNextControl((Control)sender, true, true, true, true);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
+    public class Logic
+    {
+        public static string GetOutput(int money)
+        {
+            // НАЧАЛО логики
+
+            string outmessage = "";
+            if (money < 1 || money > 9999)
+                outmessage = "Неверно введены данные";
+            else
+            {
+                int rub, kop = 0;
+                rub = money / 100;
+                kop = money % 100;
+                if (rub % 10 == 1 && rub != 11)
+                    outmessage = rub + " Рубль ";
+                else if ((rub % 10 == 2 || rub % 10 == 3 || rub % 10 == 4) && (rub < 10 || rub > 20))
+                    outmessage = rub + " Рубля ";
+                else if (rub == 0)
+                    outmessage = "";
+                else
+                    outmessage = rub + " Рублей ";
+
+                if ((kop % 10 == 2 || kop % 10 == 3 || kop % 10 == 4) && kop > 20)
+                    outmessage += kop + " Копейки";
+                else if (kop % 10 == 1 && kop != 11)
+                    outmessage += kop + " Копейка";
+                else if (kop == 0)
+                    outmessage += "ровно";
+                else
+                    outmessage += kop + " Копеек";
+            }
+            // КОНЕЦ логики
+            return outmessage;
+        }
+    }
+
+
 }
